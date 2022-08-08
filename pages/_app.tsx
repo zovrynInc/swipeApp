5d6d344492bde7cac85c448a3d03a2b0787af378
LordSpace <zovrynceo@gmail.com>
2022-08-07 19:49:26 -0500

    yaaya

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Box, ChakraProvider, HStack, useColorMode } from "@chakra-ui/react";
+import theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  // import * as React from 'react'
+  // const { colorMode, toggleColorMode } = useColorMode();
+  // console.log("this is color ", colorMode);
+  // 1. import `ChakraProvider` component
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  // function App() {
+  // 2. Wrap ChakraProvider at the root of your app
+  return (
+    <ChakraProvider theme={theme}>
+      <HStack>
+        <Box width={[0, 0.5, 0.3]} color={"black"}>
+          {" "}
+          {/* <MainSideBar> */}
+          <Component {...pageProps} />
+          {/* </MainSideBar> */}
+        </Box>
+
+        {/* <Box width={[0, 0.5, 0.3]}> </Box> */}
+      </HStack>
+    </ChakraProvider>
+  );
+  // }
 }
 
-export default MyApp
+export default MyApp;
